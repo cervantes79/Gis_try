eb5b0c2a7ad7a27d4b27a47fbaa11ec2df318890
Barış Genç <cervantes79@gmail.com>
2022-03-24 17:21:33 +0300

    pep8

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-from itsdangerous import json
 import load_data
 from flask_restful import Resource, Api, reqparse
 
@@ -9,16 +8,17 @@ app.secret_key = 'Superstar'
 api = Api(app)
 baseUrl = "/api/"
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/loaddata')
 def load():
-	return jsonify(load_data.check_db())
+    return jsonify(load_data.check_db())
 
 
 @app.route('/addresses')
@@ -27,15 +27,15 @@ def addresses():
     ne = request.args.get('ne')
     zoom = request.args.get('zoom')
     if zoom is None:
-        zoom=5.51
+        zoom = 5.51
     if sw is None:
-        sw="-11.962067381231293,51.81965717678804"
+        sw = "-11.962067381231293,51.81965717678804"
     if ne is None:
-        ne="5.31906738123115,56.60417303370079"
+        ne = "5.31906738123115,56.60417303370079"
 
-    rslt=load_data.get_data(sw,ne,zoom)
+    rslt = load_data.get_data(sw, ne, zoom)
     return jsonify(rslt)
-    
+
 
 class webAddresses(Resource):
     def __init__(self, *args, **kwargs):
@@ -47,39 +47,39 @@ class webAddresses(Resource):
 
     def get(self):
         args = self.reqparse.parse_args()
-        sw= args.get("sw")
-        ne= args.get("ne")
-        zoom= args.get("zoom")
+        sw = args.get("sw")
+        ne = args.get("ne")
+        zoom = args.get("zoom")
         if zoom is None:
-            zoom=5.51
+            zoom = 5.51
         if sw is None:
-            sw="-11.962067381231293,51.81965717678804"
+            sw = "-11.962067381231293,51.81965717678804"
         if ne is None:
-            ne="5.31906738123115,56.60417303370079"
+            ne = "5.31906738123115,56.60417303370079"
 
-        rslt=load_data.get_data(sw,ne,zoom)
+        rslt = load_data.get_data(sw, ne, zoom)
 
         return jsonify(rslt)
 
     def post(self):
         args = self.reqparse.parse_args()
-        sw= args.get("sw")
-        ne= args.get("ne")
-        zoom= args.get("zoom")
+        sw = args.get("sw")
+        ne = args.get("ne")
+        zoom = args.get("zoom")
         if zoom is None:
-            zoom=5.51
+            zoom = 5.51
         if sw is None:
-            sw="-11.962067381231293,51.81965717678804"
+            sw = "-11.962067381231293,51.81965717678804"
         if ne is None:
-            ne="5.31906738123115,56.60417303370079"
+            ne = "5.31906738123115,56.60417303370079"
 
-        rslt=load_data.get_data(sw,ne,zoom)
+        rslt = load_data.get_data(sw, ne, zoom)
 
         return jsonify(rslt)
+
 
 api.add_resource(webAddresses, baseUrl + 'address', endpoint='wAddress')
 
 
-
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=83,threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=83, threaded=True)
