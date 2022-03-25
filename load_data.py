c77a7254a7651310b5af082887630f008f6be792
Sourcery AI <>
2022-03-25 18:58:50 +0000

    'Refactored by Sourcery'

--- load_data.py
+++ load_data.py
@@ -31,9 +31,7 @@ def get_query(query):
 
     column_counts = len(colnames)
     for i in rows:
-        data = {}
-        for g in range(column_counts):
-            data[colnames[g]] = i[g]
+        data = {colnames[g]: i[g] for g in range(column_counts)}
         resultlist.append(data)
     return resultlist
 
@@ -81,10 +79,7 @@ def load_csv():
     with open('test_addresses.csv') as csvfile:
         csv_reader = csv.reader(csvfile)
         csv_rows = list(csv_reader)
-    filterted_rows = []
-    for i in csv_rows:
-        if i[7] == 'true':
-            filterted_rows.append(i)
+    filterted_rows = [i for i in csv_rows if i[7] == 'true']
     create_db_connection()
     for i in filterted_rows:
         rowid = i[0]
@@ -92,14 +87,17 @@ def load_csv():
         address1 = str(i[2])
         address2 = str(i[3])
         postcode = str(i[4])
-        geom = "ST_SetSRID(ST_MakePoint({},{}),4326)".format(i[5], i[6])
+        geom = f"ST_SetSRID(ST_MakePoint({i[5]},{i[6]}),4326)"
         number_value = str(i[8])
         address3 = str(i[9])
         address4 = str(i[10])
-        query = "insert into public.ukdata (rowid, country_code, address1, "
-        query += " address2, postcode, geom,  number_value, address3,"
+        query = (
+            "insert into public.ukdata (rowid, country_code, address1, "
+            + " address2, postcode, geom,  number_value, address3,"
+        )
+
         query += " address4)"
-        " values (%s,%s,%s,%s,%s, {},%s,%s,%s)".format(geom)
+        f" values (%s,%s,%s,%s,%s, {geom},%s,%s,%s)"
         data_list = [
                         rowid, country_code, address1,
                         address2, postcode, number_value,
@@ -115,13 +113,19 @@ def check_db():
     query = "SELECT count(*) FROM pg_database where datname='test';"
     data = get_query(query=query)
     if data[0]["count"] == 0:
-        query = "CREATE DATABASE test WITH OWNER = postgres ENCODING = 'UTF8'"
-        query += " LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' "
+        query = (
+            "CREATE DATABASE test WITH OWNER = postgres ENCODING = 'UTF8'"
+            + " LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' "
+        )
+
         query += " TABLESPACE = pg_default CONNECTION LIMIT = -1;"
         query += " CREATE EXTENSION postgis; "
         set_query2(query)
-    query = "SELECT count(*) FROM pg_catalog.pg_tables WHERE "
-    query += "schemaname != 'pg_catalog' AND schemaname "
+    query = (
+        "SELECT count(*) FROM pg_catalog.pg_tables WHERE "
+        + "schemaname != 'pg_catalog' AND schemaname "
+    )
+
     query += " != 'information_schema' and tablename='ukdata';"
     data = get_query(query=query)
     if data[0]["count"] == 0:
@@ -179,7 +183,7 @@ def get_data(sw, en, zoom):
         it returns list of dictionaries with keys below:
         [{'latitude':'','longitude':'','count':''}]
         """
-    envelope = str(sw) + "," + str(en)
+    envelope = f'{str(sw)},{str(en)}'
     query = """SELECT row_number() over () AS id,
     ST_NumGeometries(gc) as countpoint,
     ST_AsText (ST_Transform (ST_Centroid(gc), 4326)) as loca
@@ -191,12 +195,8 @@ def get_data(sw, en, zoom):
     data = get_query(query=query)
     result_list = []
     for i in data:
-        stagingdict = {}
         lat, lon = point2longlat(i["loca"])
-        stagingdict["latitude"] = lat
-        stagingdict["longitude"] = lon
-        stagingdict["count"] = i["countpoint"]
-
+        stagingdict = {"latitude": lat, "longitude": lon, "count": i["countpoint"]}
         result_list.append(stagingdict)
 
     return result_list
